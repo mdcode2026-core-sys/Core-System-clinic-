a36cf5976dc6a258a9e4cc48f2ca4fd2a3c6b2cb
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-08-27 19:53:01 +0300

    AJM-2: expose Financial Plans surface

---
+++ src/app/(dashboard)/financial-plans/page.tsx
@@ -0,0 +1,2 @@
+import { FinancialResourceListPage } from "@/features/financial-resources/financial-resource-list-page";
+export default function FinancialPlansPage() { return <FinancialResourceListPage resource="financial-plans" />; }
