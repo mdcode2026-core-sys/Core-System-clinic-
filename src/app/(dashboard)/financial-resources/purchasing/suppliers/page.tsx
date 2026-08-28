4457427cdfc018112f5e0c37c8659881bd58e25e
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-08-28 10:51:46 +0300

    refactor(AJM-2): nest suppliers under purchasing

---
+++ src/app/(dashboard)/financial-resources/purchasing/suppliers/page.tsx
@@ -0,0 +1,2 @@
+import { FinancialResourceListPage } from "@/features/financial-resources/financial-resource-list-page";
+export default function FinancialResourcesSuppliersPage() { return <FinancialResourceListPage resource="suppliers" />; }
