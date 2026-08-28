0d3b784b92f4006d625d657ee2d343eb62105aec
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-08-28 10:51:09 +0300

    refactor(AJM-2): move financial plans under Financial & Resources

---
+++ src/app/(dashboard)/financial-resources/financial-plans/page.tsx
@@ -0,0 +1,2 @@
+import { FinancialResourceListPage } from "@/features/financial-resources/financial-resource-list-page";
+export default function FinancialResourcesFinancialPlansPage() { return <FinancialResourceListPage resource="financial-plans" />; }
