47e5fb6a776e2901063c66b8739707395cc11fbc
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-08-28 10:51:51 +0300

    refactor(AJM-2): nest receiving under purchasing

---
+++ src/app/(dashboard)/financial-resources/purchasing/receiving/page.tsx
@@ -0,0 +1,2 @@
+import { FinancialResourceListPage } from "@/features/financial-resources/financial-resource-list-page";
+export default function FinancialResourcesReceivingPage() { return <FinancialResourceListPage resource="receiving" />; }
