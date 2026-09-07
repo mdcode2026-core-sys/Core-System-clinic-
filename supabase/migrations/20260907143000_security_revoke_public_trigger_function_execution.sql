052a265e4f6cd93680292ed06a0487e7b4796185
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-09-07 17:21:03 +0300

    security: remove public trigger function execution

---
+++ supabase/migrations/20260907143000_security_revoke_public_trigger_function_execution.sql
@@ -0,0 +1,6 @@
+revoke execute on function public.guard_inventory_stock_write() from public, anon;
+revoke execute on function public.guard_invoice_mutation() from public, anon;
+revoke execute on function public.guard_invoice_item_mutation() from public, anon;
+revoke execute on function public.sync_clinic_user_auth_email() from public, anon;
+revoke execute on function public.sync_supplier_bill_obligation() from public, anon;
+revoke execute on function public.validate_ajm2_tenant_integrity() from public, anon;
