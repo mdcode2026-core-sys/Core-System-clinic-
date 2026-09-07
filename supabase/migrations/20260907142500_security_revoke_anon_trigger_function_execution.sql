3615602d5878d9406f4b50974849aa7218800c60
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-09-07 17:20:13 +0300

    security: revoke anon trigger function execution

---
+++ supabase/migrations/20260907142500_security_revoke_anon_trigger_function_execution.sql
@@ -0,0 +1,4 @@
+revoke execute on function public.guard_inventory_stock_write() from anon;
+revoke execute on function public.sync_clinic_user_auth_email() from anon;
+revoke execute on function public.sync_supplier_bill_obligation() from anon;
+revoke execute on function public.validate_ajm2_tenant_integrity() from anon;
