367e5e59ba5f30b22b5e978237f73ad4bdade6d3
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-09-09 20:26:32 +0300

    chore(migrations): remove superseded stage19 ACL filename

--- supabase/migrations/20260909172610_stage19_supplier_payment_trigger_privilege_hardening.sql
+++
@@ -1,4 +0,0 @@
-BEGIN;
-REVOKE EXECUTE ON FUNCTION public.sync_supplier_obligation_from_payments() FROM PUBLIC, anon, authenticated;
-GRANT EXECUTE ON FUNCTION public.sync_supplier_obligation_from_payments() TO service_role;
-COMMIT;
